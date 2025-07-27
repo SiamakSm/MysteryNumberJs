63ca323bf17eb6a7a2b34451828fd330022b17fe
SiamakSm <siamak036@gmail.com>
2025-07-27 18:53:28 +0200

    Syntax update

--- script.js
+++ script.js
@@ -1,43 +1,49 @@
 
-let mysteryNumber = 111;
+let mysteryNumber;
+let message = document.getElementById("message");
+let message2 = document.getElementById("message2");
+let buttonGuess = document.getElementById("buttonGuess");
+let buttonStart = document.getElementById("buttonStart");
+let input = document.getElementById("input");
 disable(true);
 
-function disable(status) {
-    if (status == true) {
-        document.getElementById("buttonGuess").disabled = true;
-        document.getElementById("input").disabled = true;
-        document.getElementById("message2").textContent = "Please Start the Game!";
 
-    } else {
+function disable(status) {
+    if (status == true) {           //in case that the game is off
+        buttonGuess.disabled = true;
+        input.disabled = true;
+        message2.textContent = "Please Start the Game!";
+
+    } else {                       //in case that the game is on
         mysteryNumber = 1 + (Math.floor(Math.random() * 10));
-        document.getElementById("input").value = "A Number PLZ!";
-        document.getElementById("buttonGuess").disabled = false;
-        document.getElementById("input").disabled = false;
-        document.getElementById("message2").textContent = "";
-        document.getElementById("message").textContent = "Make a Guess!";
+        input.value = "A Number PLZ!";
+        buttonGuess.disabled = false;
+        input.disabled = false;
+        message2.textContent = "";
+        message.textContent = "Make a Guess!";
     };
 };
-console
-document.getElementById("buttonStart").addEventListener("click", function () {
+
+buttonStart.addEventListener("click", function () { // Game Start Button
     disable(false);
 });
 
-document.getElementById("buttonGuess").addEventListener("click", function () {
-    const inputValue = Number(document.getElementById("input").value);
+buttonGuess.addEventListener("click", function () { // Button to guess the mystery number
+    const inputValue = Number(input.value);
     if (isNaN(inputValue) || inputValue < 1 || inputValue > 10) {
-        document.getElementById("message").textContent = "Please enter a number between 1 and 10!";
+        message.textContent = "Please guess a number between 1 and 10!";
         return;
     };
     guess(mysteryNumber, inputValue);
 });
 
-function guess(mysteryNumber, inputValue) {
+function guess(mysteryNumber, inputValue) { // To compare our number with the mystery number
     if (inputValue == mysteryNumber) {
-        document.getElementById("message").textContent = "Yeeees !!! You Won !";
+        message.textContent = "Yeeees !!! You Won !";
         disable(true);
     } else if (inputValue > mysteryNumber) {
-        document.getElementById("message").textContent = "Noo !!! Try A Smaller One !";
+        message.textContent = "Noo !!! Try A Smaller One !";
     } else {
-        document.getElementById("message").textContent = "Noo !!! Try A Bigger One !";
+        message.textContent = "Noo !!! Try A Bigger One !";
     };
 };
